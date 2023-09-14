d4b4c010438f975dd9050b2be643855bc5a83553
Pankaj <cdeveloper2016@gmail.com>
2023-09-14 18:47:46 +0530

    Integrated the API Data in Energy,Tariff Details components

--- src/components/RenderDetails/EnergyDesc.js
+++ src/components/RenderDetails/EnergyDesc.js
@@ -10,43 +10,49 @@ export default function EnergyDesc({ data, RenderPieChart }) {
   return (
     <>
       <Modal open={openModal} setOpen={setOpenModal}>
-        <TariffDetails />
+        <TariffDetails data={data} />
       </Modal>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} lg={3}>
           <Typography sx={style.title}>Tariff: {data?.name}</Typography>
-          <Typography sx={{ mt: 1,...styles.p }}>
+          <Typography sx={{ mt: 1, ...styles.p }}>
             Preisgarantie:{" "}
             {data?.priceGuarantee.period
               ? `${data?.priceGuarantee.period.value} ${data?.priceGuarantee.period.unit}`
               : "Keine"}
           </Typography>
-          <Typography sx={{ mt: 1,...styles.p }}>
+          <Typography sx={{ mt: 1, ...styles.p }}>
             Kündigungsfrist:{" "}
             {`${data?.cancellationPeriod.value} ${data?.cancellationPeriod.unit}`}
           </Typography>
-          <Typography sx={{ mt: 1,...styles.p }}>
+          <Typography sx={{ mt: 1, ...styles.p }}>
             Mindestvertragslaufzeit:{" "}
             {data?.minimumTerm
               ? `${data?.minimumTerm.value} ${data?.minimumTerm.unit}`
               : "Keine"}
           </Typography>
           <Box sx={{ mt: 1 }}>
-            <Typography sx={styles.p}>Grundpreis: {data?.price.basePrice} €/Monat</Typography>
+            <Typography sx={styles.p}>
+              Grundpreis: {data?.price.basePrice} €/Monat
+            </Typography>
             <Typography sx={styles.p}>
               Arbeitspreis: {data?.price.workingPrice} €/Monat
             </Typography>
             <Typography sx={styles.p}>
-              Gesamtpreis:{" "}
+              Gesamtpreis:
               {Math.round(
                 (data?.price.basePrice + data?.price.workingPrice) * 100
-              ) / 100}{" "}
+              ) / 100}
               €/Monat
             </Typography>
             <Link onClick={() => setOpenModal(true)}>
               <Box display="flex" alignItems="center" mt={2}>
                 <InfoOutlinedIcon variant="outlined" sx={{ color: "#000" }} />
-                <Typography variant="p" fontSize={14} sx={{ color: "#000",...styles.p }}>
+                <Typography
+                  variant="p"
+                  fontSize={14}
+                  sx={{ color: "#000", ...styles.p }}
+                >
                   TARIF-DETAILS
                 </Typography>
               </Box>
@@ -99,13 +105,17 @@ export default function EnergyDesc({ data, RenderPieChart }) {
             Vorteile: {data?.provider.name}
           </Typography>
           <Typography sx={styles.p}>
-          <div dangerouslySetInnerHTML={{ __html: data?.advantages }}></div>
+            <div dangerouslySetInnerHTML={{ __html: data?.advantages }}></div>
           </Typography>
         </Grid>
         <Grid item xs={12} sm={6} lg={3}>
           <Typography sx={style.title}>Labels</Typography>
           {data?.labels.map((label, i) => {
-            return <Typography key={i} sx={styles.p}>{label.authority}</Typography>;
+            return (
+              <Typography key={i} sx={styles.p}>
+                {label.authority}
+              </Typography>
+            );
           })}
         </Grid>
       </Grid>
@@ -117,6 +127,6 @@ const style = {
   title: {
     fontWeight: 900,
     fontSize: "20px",
-    ...styles.p
+    ...styles.p,
   },
 };
